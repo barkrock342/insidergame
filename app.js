5dc4068a1412594d9e77be5d190459e7804aee12
Markrock342 <markrock342@gmail.com>
2025-12-05 18:28:14 +0700

    2

--- app.js
+++ app.js
@@ -370,4 +370,7 @@ io.sockets.on('connection', function (socket) {
  
 }) 
  
-server.listen(8080);
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
